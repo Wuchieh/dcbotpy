eaf891b4432a777ae04667f7dd57d7cf435f4b31
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-07-28 04:27:49 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print("test")
+import discord
+from discord.ext import commands
+import json
+
+with open('setting.json','r',encoding='utf8') as jset:
+    jdata = json.load(jset)
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('>>bot is online')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(F'{round(bot.latency*1000)} 毫秒')
+
+bot.run(jdata['TOKEN'])
