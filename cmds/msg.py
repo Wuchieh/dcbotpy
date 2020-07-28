874565a1e390d78380ac014250a9a94647e94737
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-07-29 05:26:53 +0800

    Update msg.py

--- cmds/msg.py
+++ cmds/msg.py
@@ -12,7 +12,8 @@ class Msg(Cog_Extension):
     @commands.command()
     async def clear(self,ctx,num:int):
         await ctx.channel.purge(limit=num+1)
-        print(str(ctx.message.author)+'在'+str(ctx.channel.id)+'使用了clear指令刪除了'+str(int(num))+'個對話')
+        print(str(ctx.message.author)+' ---ID '+str(ctx.message.author.id)+
+        '在 << '+str(ctx.channel.name)+' >> 頻道使用了clear指令刪除了'+str(int(num))+'個對話')
 
 def setup(bot):
     bot.add_cog(Msg(bot))
