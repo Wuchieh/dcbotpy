b8022fd670e7ff746d6bb26647b107734852e20d
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-07-28 19:59:41 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import json
 import random
+import os
 
 with open('setting.json','r',encoding='utf8') as jset:
     jdata = json.load(jset)
@@ -11,8 +12,24 @@ bot = commands.Bot(command_prefix=jdata['command_prefix'])
 @bot.event
 async def on_ready():
     print('>>bot is online')
-@bot.command()
-async def ping(ctx):
-    await ctx.send(F'{round(bot.latency*1000)} 毫秒')
 
-bot.run(jdata['TOKEN'])
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(F'cmds.{extension}')
+    await ctx.send(F'Loaded {extension} done.')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(F'cmds.{extension}')
+    await ctx.send(F'Un - Loaded {extension} done.')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(F'cmds.{extension}')
+    await ctx.send(F'Re - Loaded {extension} done.')
+
+for Filename in os.listdir('./cmds'):
+    if Filename.endswith('.py'):
+        bot.load_extension(F'cmds.{Filename[:-3]}')
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
