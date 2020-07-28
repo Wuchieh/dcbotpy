52942e5267fbf62eaabc0de9a5648a1741a09f81
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-07-28 23:52:39 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,14 +1,19 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
+import random
 
 class Main(Cog_Extension):
-    def __init__(self,bot):
-        self.bot = bot 
 
     @commands.command()
     async def ping(self,ctx):
         await ctx.send(F'{round(self.bot.latency*1000)} 毫秒')
+
+    @commands.command()
+    async def ran(self,ctx):
+        rannum = random.randint(1,10)
+        await ctx.send(str(rannum) + str(ctx.message.id))
+        
 
 def setup(bot):
     bot.add_cog(Main(bot))
