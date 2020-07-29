cd176e2de29fa8114c8fd91834f454986362f56c
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-07-29 08:56:58 +0800

    Update test.py

--- cmds/test.py
+++ cmds/test.py
@@ -5,19 +5,7 @@ from core.classes import Cog_Extension
 class test(Cog_Extension):
     @commands.command()
     async def test(self,ctx):
-        mes = str("")
-        memberlist = ctx.guild.members
-        onlinelist = []
-        for i in memberlist:
-            if str(i.status) == "online":
-                mes = str(mes) + str(i) +' 在線\n'
-                onlinelist.append(i.name)
-            else:
-                mes = str(mes) + str(i) +' 離線\n'
-            #print(mes)
-        await ctx.send(mes)
-        for i2 in onlinelist:
-            print(i2)
+        pass
 
 def setup(bot):
     bot.add_cog(test(bot))
